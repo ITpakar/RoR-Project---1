9f535c36d69e57d2680f73c8f3147159198d505b
Tim Reynolds <tim.reynolds@codesquad.com.au>
2016-02-08 13:20:25 +0800

    update db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-Code.create(
-[
-  {name: 'Test Match', default_innings: 2}, 
-  {name: 'T20', default_innings: 1}, 
-  {name: 'ODI', default_innings: 1}
-])
+#Code.create(
+#[
+#  {name: 'Test Match', default_innings: 2}, 
+#  {name: 'T20', default_innings: 1}, 
+#  {name: 'ODI', default_innings: 1}
+#])
 
-# User.create(email: "tipping@example.com", password: "password")
+User.create(email: "tipping@example.com", password: "password")
