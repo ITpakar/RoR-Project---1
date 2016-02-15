7dfb1e2b33913a919a97027f5ec2be1e6b46c3a7
Kyle Goodall <kyle.goodall@codesquad.com.au>
2016-02-15 10:18:44 +0800

    Updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,5 @@ Code.create(
 # Admin Creation
 admin = Admin.create(:email => 'simranjit.singh@mobiloitte.com', :password => '12345678', :password_confirmation => '12345678')
 admin.build_profile.save(:validate => false)
+admin = Admin.create(:email => 'tipping@example.com', :password => 'password', :password_confirmation => 'password')
+admin.build_profile.save(:validate => false)
