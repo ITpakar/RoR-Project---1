f373b3c71869bcfc3e5bb9219535a4fc9ce2a62a
Tim Reynolds <tim.reynolds@codesquad.com.au>
2016-02-23 11:04:49 +0800

    add city to location name helper method

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,10 @@ class Location < ActiveRecord::Base
   belongs_to :country
 
   def full_name
-    "#{self.country.name} - #{self.name}"
+    if !self.city.blank?
+      "#{self.country.name} - #{self.city} - #{self.name}"
+    else
+      "#{self.country.name} - #{self.name}"  
+    end
   end
 end
