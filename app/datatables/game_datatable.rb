968fc186b1917b2b322753ff7af74199c672a8f5
Andika Bijaya <andikabijaya@gmail.com>
2016-03-14 04:00:45 +0800

    Setup Game State task completed

--- app/datatables/game_datatable.rb
+++ app/datatables/game_datatable.rb
@@ -1,6 +1,6 @@
 class GameDatatable < AjaxDatatablesRails::Base
 
-  def_delegators :@view, :link_to, :h, :mailto, :game_path, :edit_game_path
+  def_delegators :@view, :link_to, :h, :mailto, :game_path, :edit_game_path, :toggle_state_game_path, :lock_out_game_path
 
   def sortable_columns
     @sortable_columns ||= ['Game.match_date', 'Game.name', 'Location.name', 'Country.name', 'countries_squads.name']
@@ -10,12 +10,28 @@ class GameDatatable < AjaxDatatablesRails::Base
     @searchable_columns ||= ['Game.match_date', 'Game.name']
   end
 
+  def called_by
+    @called_by ||= options[:called_by]
+  end
+
   private
 
   def data
     records.map do |record|
 	    ops = ''
-      ops = link_to('<i class="fa fa-eye"> </i>'.html_safe, game_path(record))	        
+      ops = link_to('<i class="fa fa-eye"> </i>'.html_safe, game_path(record))
+      if @called_by == "index"
+        if record.completed?
+          ops = ops + " " + link_to('<i class="fa fa-plus"> </i>'.html_safe, toggle_state_game_path(record), method: :put, remote: true)
+        else
+          ops = ops + " " + link_to('<i class="fa fa-check"> </i>'.html_safe, toggle_state_game_path(record), method: :put, remote: true)
+        end
+      else
+        unless record.locked_out?
+          ops = ops + " " + link_to('<i class="fa fa-lock"> </i>'.html_safe, lock_out_game_path(record), method: :put, remote: true, data: { confirm: 'Are you sure you want to lock out this game?' })
+        end
+      end
+
 	    # ops = ops + " " + link_to('<i class="fa fa-pencil"> </i>'.html_safe, edit_game_path(record), remote: true) 
       # ops = ops + " " + link_to('<i class="fa fa-times-circle"> </i>'.html_safe, game_path(record), method: :delete, remote: true, data: { confirm: 'Are you sure you want to delete this game?' }) 
       [
